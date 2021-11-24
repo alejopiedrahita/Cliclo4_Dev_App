0f826fced10438ca5c6ea28bc08f7ac45bb7e356
hernan_naranjo <hnaranjo85@programmer.net>
2021-11-23 22:21:36 -0500

    Sprint2: Implementación en home.java de la interface View.OnClickListener con su método OnCreate para controlar los diferentes eventos de los botones. Creación de la clase ValidaciónCampos con los métodos formRegistros, formBusqueda y formEtiqueta con sus respectivas validaciones

--- app/src/main/java/com/example/finalwireframe/Home.java
+++ app/src/main/java/com/example/finalwireframe/Home.java
@@ -3,26 +3,27 @@ package com.example.finalwireframe;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class home extends AppCompatActivity implements View.OnClickListener {
+public class Home extends AppCompatActivity implements View.OnClickListener {
 
     private Button btnRegisterPlace, btnFindPlace, btnReaderCode, btnFavoritePlaces;
-
 
     public void proccessEvents(int key) {
         switch (key) {
             case R.id.btnRegisterPlace:
-                Intent intentRegister = new Intent(home.this, RegisterPlace.class);
+                Intent intentRegister = new Intent(Home.this, RegisterPlace.class);
                 startActivity(intentRegister);
                 break;
             case R.id.btnFindPlace:
-                Intent intentFind = new Intent(home.this, FindPlace.class);
+                Intent intentFind = new Intent(Home.this, FindPlace.class);
                 startActivity(intentFind);
                 break;
             case R.id.btnReaderCode:
-                Intent intentReader = new Intent(home.this, ReaderCode.class);
+                Intent intentReader = new Intent(Home.this, ReaderCode.class);
                 startActivity(intentReader);
                 break;
         }
